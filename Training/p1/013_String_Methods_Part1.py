87e6b825347dd1688ed9741e4b510ef4202ab0d9
Meshari <almeshari93@gmail.com>
2021-03-24 23:03:03 +0300

    print_function

--- Training/p1/013_String_Methods_Part1.py
+++ Training/p1/013_String_Methods_Part1.py
@@ -3,5 +3,53 @@
 # ------------------------------
 
 a = "I Love Python"
+b = "     I Love  Python     "
+c = "####I Love  Python#####"
 
-print(a)
+print(len(a))
+print(len(b))
+
+# Strip() rstrip() lstrip()
+
+print(b.strip())
+print(b.lstrip())
+print(b.rstrip())
+
+print("---------------- \n")
+print(c.strip("#"))
+print(c.rstrip("#"))
+print(c.lstrip("#"))
+
+
+#Title() 
+
+b = "I Love Graphics and 3g Technology and python"
+print(b.title())
+
+# capitalize()
+
+b = "I Love 2d Graphics and 3g Technology and Python"
+print(b.capitalize())
+
+# zfile  
+
+c, d , e, f ="1", "11", "111", "1111"
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+print(c.zfill(4))
+print(d.zfill(4))
+print(e.zfill(4))
+print(e.zfill(4))
+
+# upper()
+
+g = "meshari"
+print(g.upper())
+
+# lower()
+h = "MESHARI"
+print(h.lower())
